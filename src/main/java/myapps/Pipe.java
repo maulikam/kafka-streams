895ed790a50037f10fdc3bbbec088f2a81f410f2
Maulik Dave <mdave.5191@gmail.com>
2024-06-04 15:55:27 +0530

    basic structure

--- src/main/java/myapps/Pipe.java
+++ src/main/java/myapps/Pipe.java
@@ -12,28 +12,24 @@ import java.util.concurrent.CountDownLatch;
 
 public class Pipe {
 
-    public static void main(String[] args) {
-        Properties properties = new Properties();
-        properties.put(StreamsConfig.APPLICATION_ID_CONFIG, "streams-pipe");
-        properties.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-
-        properties.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
-        properties.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+    public static void main(String[] args) throws Exception {
+        Properties props = new Properties();
+        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "streams-pipe");
+        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+        props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
 
         final StreamsBuilder builder = new StreamsBuilder();
-        KStream<String, String> source =
-                builder.stream("streams-plaintext-input");
-        source.to("streams-pipe-output");
+
+        builder.stream("streams-plaintext-input").to("streams-pipe-output");
 
         final Topology topology = builder.build();
-        System.out.println(topology.describe());
 
-        KafkaStreams streams = new KafkaStreams(topology, properties);
-
-
+        final KafkaStreams streams = new KafkaStreams(topology, props);
         final CountDownLatch latch = new CountDownLatch(1);
 
-        Runtime.getRuntime().addShutdownHook(new Thread("stream-shutdown-hook"){
+        // attach shutdown handler to catch control-c
+        Runtime.getRuntime().addShutdownHook(new Thread("streams-shutdown-hook") {
             @Override
             public void run() {
                 streams.close();
@@ -41,19 +37,13 @@ public class Pipe {
             }
         });
 
-
         try {
             streams.start();
-            latch.wait();
-        } catch (InterruptedException e) {
+            latch.await();
+        } catch (Throwable e) {
             System.exit(1);
         }
-
         System.exit(0);
-
-
-
-
     }
 
 }
